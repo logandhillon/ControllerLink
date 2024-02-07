4e9ff7ff4626a6d8f1142138c6449574999237fb
Logan Dhillon <logandhi64@gmail.com>
2024-02-06 23:49:06 -0500

    Try to get JInput to work

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ version = "0.1.0-dev"
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.logging.log4j:log4j-core:2.22.1")
-    implementation("uk.co.electronstudio.sdl2gdx:sdl2gdx:1.0.4")
+    implementation("us.ihmc:jinput:2.0.6-ihmc2")
+    implementation("net.java.jinput:jinput-platform:2.0.7")
 }
 
 tasks.test {
