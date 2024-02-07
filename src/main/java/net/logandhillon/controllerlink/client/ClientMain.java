95d641e5500793cae7e1ba28bfab91828aba5722
Logan Dhillon <logandhi64@gmail.com>
2024-02-06 19:11:26 -0500

    Print socket input stream to stdout

--- src/main/java/net/logandhillon/controllerlink/client/ClientMain.java
+++ src/main/java/net/logandhillon/controllerlink/client/ClientMain.java
@@ -22,7 +22,9 @@ import net.logandhillon.controllerlink.server.ServerMain;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -35,11 +37,16 @@ public final class ClientMain {
         InetSocketAddress address = getSocketAddress(args);
         if (address == null) throw new NullPointerException("Could not find target! Do not forget to pass a target IP with --target xxx.xxx.xxx.xxx");
 
-        LOG.info("Connecting to {}", address);
-
         try (Socket socket = new Socket()) {
             socket.connect(address);
-            LOG.info("Connected to {}", socket);
+            LOG.info("Connected to {}", address);
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    LOG.info("Server says: " + inputLine);
+                }
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
